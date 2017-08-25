b30bc6547b824bbe363fd8e7ede55b1c76864d1c
lbias <306508354@qq.com>
2017-08-25 12:08:35 +0800

    model driver_session validations

--- app/models/driver_session.rb
+++ app/models/driver_session.rb
@@ -1,11 +1,17 @@
 # tableless model, to handle driver session logic
 class DriverSession
+	include ActiveModel::Validations
+	include ActiveModel::Conversion
 	extend ActiveModel::Naming
   attr_accessor :email, :password, :token
+
+	validates_presence_of :email, :password
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   def initialize(email=nil, password=nil)
     @email = email
     @password = password
+		# TODO call API to authenticate
   end
 
   def to_model
--- spec/models/driver_session_spec.rb
+++ spec/models/driver_session_spec.rb
@@ -4,22 +4,22 @@ describe DriverSession, type: :model do
 	describe "initialization" do
     it "should has email and passwords in proper format" do
 			driver_session = build(:driver_session)
-      expect( driver_session.valid? ).to_be true
+      expect( driver_session ).to be_valid
 
       driver_session = build(:driver_session, email: nil)
-      expect( driver_session.valid? ).to_be false
+      expect( driver_session ).not_to be_valid
 
       driver_session = build(:driver_session, password: nil)
-      expect( driver_session.valid? ).to_be false
+      expect( driver_session ).not_to be_valid
 
       driver_session = build(:driver_session, email: 'invalid format')
-      expect( driver_session.valid? ).to_be false
+      expect( driver_session ).not_to be_valid
 		end
 
     it "should authenitcate with the api and retrieve a token" do
 			driver_session = build(:driver_session) # has 4 items with identical periods
 
-      expect( driver_session.token ).not_to be nil      
+      expect( driver_session.token ).not_to be nil
 		end
   end
 end
